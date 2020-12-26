2f95152d74fbf5e25b053d864038eaa98edd07e6
komura <affirman03139@outlook.jp>
2020-12-26 16:45:23 +0900

    fix(router): 命名によるエラーを修正

--- router.go
+++ router.go
@@ -28,22 +28,22 @@ func main() {
 func serve() {
     // デフォルトのミドルウェアでginのルーターを作成
     // Logger と アプリケーションクラッシュをキャッチするRecoveryミドルウェア を保有しています
-	router := gin.Default()
-	melody := melody.New()
+	ginRouter := gin.Default()
+	melodyInstance := melody.New()
 
     // 静的ファイルのパスを指定
-	router.Static("/view", "./view")
+	ginRouter.Static("/view", "./view")
 	
 	// HTML読み込み,これによりテンプレートを使用できる
-	router.LoadHTMLGlob("view/*.html")
+	ginRouter.LoadHTMLGlob("view/*.html")
 
 	// index.htmlはr.LoadHTMLGlob()でロードされているファイルから読み込む
-	router.GET("/", func(c *gin.Context) {
+	ginRouter.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 	
 	// /(部屋を示す文字列)にリクエストが来たらroom.htmlを返す
-	router.GET("room/:name", func(c *gin.Context) {
+	ginRouter.GET("room/:name", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "room.html", gin.H{
 			"Room_Name": c.Param("room_name"),
 		})
@@ -51,23 +51,21 @@ func serve() {
 
 	// /:name/wsにリクエストが来ると，webSocketの通信として、HandleMessageの処理を行う
 	// HandleRequestは、httpリクエストをWebSocket接続にし、melodyインスタンスによって処理されるようにディスパッチする
-	router.GET("room/:name/ws", func(c *gin.Context) {
-		melody.HandleRequest(c.Writer, c.Request)
+	ginRouter.GET("room/:name/ws", func(c *gin.Context) {
+		melodyInstance.HandleRequest(c.Writer, c.Request)
 	})
 
-	router.GET("/generateRN",indexController.GenerateRN)
+	ginRouter.GET("/generateRN",indexController.GenerateRN)
 
 	// 部屋名(URL)が同じクライアントのみに，送られてきた値を送信する
-	/*
-	melody.HandleMessage(func(s *melody.Session, msg []byte) {
-		melody.BroadcastFilter(msg, func(q *melody.Session) bool {
+	melodyInstance.HandleMessage(func(s *melody.Session, msg []byte) {
+		melodyInstance.BroadcastFilter(msg, func(q *melody.Session) bool {
 			return q.Request.URL.Path == s.Request.URL.Path
 		})
 	})
-*/
 	
 	// 8080ポートで待ち受ける
-    if err := router.Run(":8080"); err != nil {
+    if err := ginRouter.Run(":8080"); err != nil {
         log.Fatal("Server Run Failed.: ", err)
     }
 }
